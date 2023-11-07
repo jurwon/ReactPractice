360a730c048b425019a58832ebcf49fda2feb39b
jurwon <susorrow@naver.com>
2023-11-07 14:31:28 +0900

    useState로 style변경해보기

--- ch3-test/src/Component/MySay.js
+++ ch3-test/src/Component/MySay.js
@@ -6,6 +6,7 @@ const MySay = () => {
   //const[상태값,세터함수] = useState('')->반환 배열 형식
   const [username, setUserName] = useState("sjw");
   const [password, setPassword] = useState("1234");
+  const [color, setColor] = useState("skyblue");
 
   //이벤트 핸들러 추가. onClick
   const onClickUserName = () => setUserName("손주원");
@@ -13,12 +14,18 @@ const MySay = () => {
 
   return (
     <div>
-      <h1>{username} 님 환영합니다</h1>
+      <h1 style={{ color }}>{username} 님 환영합니다</h1>
       <h1>
         {username} 님 패스워드 확인 테스트 : {password}
       </h1>
-      <button onClick={onClickUserName}>username확인하기</button>
-      <button onClick={onClickPassword}>password확인하기</button>
+      <button onClick={onClickUserName}>username</button>
+      <button onClick={onClickPassword}>password</button>
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        red
+      </button>
+      <button style={{ color: "skyblue" }} onClick={() => setColor("skyblue")}>
+        skyblue
+      </button>
     </div>
   );
 };
