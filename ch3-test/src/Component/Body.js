f247b0721ac9e1ace0597d08e10a2c4650d97698
jurwon <susorrow@naver.com>
2023-11-07 12:27:59 +0900

    state를 활용하여 플러스/마이너스 버튼 생성

--- ch3-test/src/Component/Body.js
+++ ch3-test/src/Component/Body.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Test from "./test";
+import FuncTest from "./FuncTest";
+import ClassTest from "./ClassTest";
+import "../CSS/test.css";
+
+import MyComp from "./MyComp";
+import MyCompPublicFunction from "./MyCompPublicFunction";
 
 const Body = () => {
   return (
-    <div>
+    <div className="body">
       <h1>Body</h1>
+      <Test />
+      <FuncTest />
+      <ClassTest />
+
+      {/* 자식컴포넌트에게 props.name전달 */}
+      {/* <MyComp name="sjw" pw="1234" /> */}
+      <MyComp name="sjw" pw="1234">
+        여긴 MyComp
+      </MyComp>
+
+      <MyCompPublicFunction name="sjw" pw="1234">
+        여긴 MyCompPublicFunction
+      </MyCompPublicFunction>
     </div>
   );
 };
