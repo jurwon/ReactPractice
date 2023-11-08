28d95c5ddf94dc375e7f7b53bfb0a2d19d79b436
jurwon <susorrow@naver.com>
2023-11-08 12:20:51 +0900

    styled-components 사용 연습1

--- ch4-test/src/component/Main.js
+++ ch4-test/src/component/Main.js
@@ -1,15 +1,25 @@
 import { Button } from "antd";
 import React from "react";
+import styled from "styled-components";
 
 //yarn add react-router-dom으로 도구 설치
 import { useNavigate } from "react-router-dom";
 
+const MainTitleTextCss = styled.p`
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  color: orange;
+`;
+
 const Main = () => {
   const navigate = useNavigate();
+  //styled.원하는 태그 선택 '(백틱) '(백틱), 열고 닫고
 
   return (
     <div className="main">
       <h1>메인 페이지</h1>
+      <MainTitleTextCss>styled components test</MainTitleTextCss>
       <Button title="회원가입하기" onClick={() => navigate("/Join")}>
         회원가입하기
       </Button>
