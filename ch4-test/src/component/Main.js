341ec5ac998d60484b91f4462d133c9cdde10536
jurwon <susorrow@naver.com>
2023-11-08 12:40:26 +0900

    styled-components 사용 연습2

--- ch4-test/src/component/Main.js
+++ ch4-test/src/component/Main.js
@@ -9,7 +9,32 @@ const MainTitleTextCss = styled.p`
   font-size: 30px;
   font-weight: bold;
   text-align: center;
+  color: #ffdfdf;
+`;
+
+const MainTextCss = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
   color: orange;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  width: calc(100%-40px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 720px;
+  & {
+    :not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
 `;
 
 const Main = () => {
@@ -17,17 +42,21 @@ const Main = () => {
   //styled.원하는 태그 선택 '(백틱) '(백틱), 열고 닫고
 
   return (
-    <div className="main">
-      <h1>메인 페이지</h1>
-      <MainTitleTextCss>styled components test</MainTitleTextCss>
-      <Button title="회원가입하기" onClick={() => navigate("/Join")}>
-        회원가입하기
-      </Button>
-      <br />
-      <Button title="MyCount" onClick={() => navigate("/MyCount")}>
-        MyCount
-      </Button>
-    </div>
+    <Wrapper>
+      <div>
+        <h1>메인 페이지</h1>
+        <Container>
+          <MainTitleTextCss>styled components test</MainTitleTextCss>
+          <Button title="회원가입하기" onClick={() => navigate("/Join")}>
+            회원가입하기
+          </Button>
+          <br />
+          <Button title="MyCount" onClick={() => navigate("/MyCount")}>
+            MyCount
+          </Button>
+        </Container>
+      </div>
+    </Wrapper>
   );
 };
 
