125177f18b23e746b9ac3805c73ced5b413f3410
jurwon <susorrow@naver.com>
2023-11-09 11:50:49 +0900

    useEffect

--- ch5-test/src/ch8_component/InfoTestUseEffect.js
+++ ch5-test/src/ch8_component/InfoTestUseEffect.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const InfoTestUseEffect = () => {
-  console.log("useEffect호출됨");
-  console.log({
-    name,
-    nickname,
-  });
-
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  // useEffect Test 하기.
+  //정의 useEffect(콜백함수,의존성 배열)
+  // 의존성 배열 모양 1) 아무것도 없을 때, 매번 콜백함수 실행되고,
+  // 2) [] , 빈 배열, 한번만 실행되고
+  // 3) [list] , list의 상태가 변경 될 때 마다, 콜백함수 실행이 됨.
+  useEffect(() => {
+    console.log("useEffect 호출이됨. ");
+    console.log({
+      name,
+      nickname,
+    });
+  });
 
   //이벤트 핸들러
   const onChangeName = (e) => {
