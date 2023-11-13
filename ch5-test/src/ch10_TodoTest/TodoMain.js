297ef524db6f281567f5d32fee74fa0106e82b9d
jurwon <susorrow@naver.com>
2023-11-13 14:55:38 +0900

    리듀서 함수로 성능 개선

--- ch5-test/src/ch10_TodoTest/TodoMain.js
+++ ch5-test/src/ch10_TodoTest/TodoMain.js
@@ -3,7 +3,7 @@
 // 전체 가운데 요소로 정렬 시켜주는 템플릿
 // 1)제목 2) 입력란 3) 리스트 4) 리스트의 아이템 등.
 
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback, useReducer } from "react";
 import styled from "styled-components";
 import TodoHeader from "./TodoHeader";
 import TodoInsert from "./TodoInsert";
@@ -51,12 +51,41 @@ const TodoMain = () => {
     return array;
   };
 
-  const [todos, setTodos] = useState(createBulkTodos());
+  //추가로 입력이 되는 todo 부분의 아이디를 id : 4부터 할당 할 예정.
+  // const nextId = useRef(4);
 
-  const nextId = useRef(4);
+  // 3000개 더미 데이터라서, 다음 번호
+  const nextId = useRef(30001);
 
   //onInsert 함수는, onChange함수와 다르게 매번 새로운 함수 생성
   //함수 안에 배열이 변경돼서
+
+  //방법2) 성능상 큰 차이 없고, 규모가 커지고, 상태를 관리해주는 라이브러리 Redux를 사용하게 되면
+  //useReducer를 이용하게 됩니다.
+
+  //순서1) useReducer임포트
+  // import {useReducer} from "react"
+  //준비물 . 1)리듀서 함수, 2) useReducer생성, 3)dispatch함수 호출
+
+  //1) 리듀서 함수
+  const todoReducer = (todos, action) => {
+    switch (action.type) {
+      case "INSERT":
+        return todos.concat(action.todo);
+      case "REMOVE":
+        return todos.filter((todo) => todo.id !== action.id);
+      case "TOGGLE":
+        return todos.map((todo) =>
+          todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+        );
+      default:
+        return todos;
+    }
+  };
+
+  // 2) useReducer 생성 -> 기존에 더미 데이터를 만드는 부분이 있어서, 위에 부분 주석하기.
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
   const onInsert = useCallback(
     (text) => {
       const todo = {
@@ -71,7 +100,11 @@ const TodoMain = () => {
       //차이점, useCallback 의존성 배열이 값 변경될 때마다 새로 함수 생성하는 부분을
       // 기존의 값으로 변경 -> 함수 형태로 변경
       //매번 새롭게 함수를 생성할 필요 없음
-      setTodos((todos) => todos.concat(todo));
+      // setTodos((todos) => todos.concat(todo));
+
+      // 3) dispatch 함수를 호출.
+      dispatch({ type: "INSERT", todo });
+
       nextId.current += 1;
     },
     //성능 개선 2번째
@@ -83,7 +116,10 @@ const TodoMain = () => {
     (id) => {
       //성능 개선 2번째
       //매번 새롭게 함수를 생성할 필요 없음
-      setTodos((todos) => todos.filter((todo) => todo.id !== id));
+      // setTodos((todos) => todos.filter((todo) => todo.id !== id));
+
+      // 3) dispatch 함수를 호출.
+      dispatch({ type: "REMOVE", id });
     },
     // [todos]
     []
@@ -93,14 +129,17 @@ const TodoMain = () => {
     (id) => {
       //성능 개선 2번째
       //매번 새롭게 함수를 생성할 필요 없음
-      setTodos(
-        // todos.map((todo) =>
-        //   todo.id === id ? { ...todo, checked: !todo.checked } : todo
-        (todos) =>
-          todos.map((todo) =>
-            todo.id === id ? { ...todo, checked: !todo.checked } : todo
-          )
-      );
+      // setTodos(
+      //   // todos.map((todo) =>
+      //   //   todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      //   (todos) =>
+      //     todos.map((todo) =>
+      //       todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      //     )
+      // );
+
+      // 3) dispatch 함수를 호출.
+      dispatch({ type: "TOGGLE", id });
     },
     // [todos]
     []
